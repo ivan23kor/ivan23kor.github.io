ad6369c5335c0cfefb4ff3b1625e28925ecec5d2
Ivan Korostelev <ivan23kor@gmail.com>
2021-03-31 22:12:43 -0600

    Revert "The last possible date"
    This reverts commit a2efc21a5dff63c64b6b6824b880a5e0355ee4cc.

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,9 +1,8 @@
 function setCountdown(date) {
-  var total_minutes = Math.floor((
-    new Date(date).getTime() - new Date().getTime()) / 60000);
-  var hours = Math.floor(total_minutes / 60);
-  var minutes = total_minutes % 60;
-  document.getElementById("countdown").innerHTML = hours + " hours & " + minutes + " minutes ";
+  var distance = new Date(date).getTime() - new Date().getTime();
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  document.getElementById("countdown").innerHTML = days + " days & " + hours + " hours ";
   document.getElementById("date").innerHTML = "Until " + date;
 }
 function runCountdown(targetDate, delay=1000) {
